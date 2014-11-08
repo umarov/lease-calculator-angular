048f76370a8641f5a87ab0bb2abab89612412c7d
Muzafar Umarov <umarov@gmail.com>
2014-11-08 16:14:25 -0500

    Add material design using angular material

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,33 +8,42 @@
  * Controller of the leaseCalculatorApp
  */
 angular.module('leaseCalculatorApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $mdToast) {
+
+    $scope.toastPosition = {
+      bottom: true,
+      top: false,
+      left: false,
+      right: true
+    };
 
     $scope.customerInfo       = true;
     $scope.carInfo            = false;
     $scope.leaseInfo          = false;
     $scope.leaseSummary       = false;
     $scope.overrageMessage    = false;
+    $scope.possibleBudget     = true;
     $scope.milesDrivenPerYear = 0.0;
     $scope.estimatedCharges   = 0.0;
     $scope.suggestedBudget    = 0.0;
     $scope.estimatedTotalMiles = 0;
     $scope.differenceInMiles   = 0;
+    $scope.demoMessage         = 'Your info isn\'t stored, it\'s for demo only';
 
 
     function Lease() {
-      this.maxMiles            = 0;
-      this.costPerMile         = 0.0;
-      this.lengthOfLease       = 0;
-      this.currentMonthInLease = 0;
+      this.maxMiles            = null;
+      this.costPerMile         = null;
+      this.lengthOfLease       = null;
+      this.currentMonthInLease = null;
       this.car                 = null;
     }
 
     function Car() {
       this.make  = '';
       this.model = '';
-      this.year  = 0;
-      this.miles = 0;
+      this.year  = null;
+      this.miles = null;
     }
 
     function Customer() {
@@ -84,28 +93,85 @@ angular.module('leaseCalculatorApp')
       return true;
     };
 
+    $scope.getToastPosition = function() {
+      return Object.keys($scope.toastPosition)
+        .filter(function(pos) { return $scope.toastPosition[pos]; })
+        .join(' ');
+    };
+
+    $scope.toastIt = function() {
+      $mdToast.show({
+        template: '<md-toast>Please enter all the information before continuing</md-toast>',
+        hideDelay: 2000,
+        position: $scope.getToastPosition()
+      });
+    };
+
+    var customerInfoDone = function() {
+      var firstName = $scope.customer.firstName;
+      var lastName  = $scope.customer.lastName;
+      var email     = $scope.customer.email;
+      return firstName !== '' && 
+             lastName !== '' && 
+             email !== '';
+    };
+
+    var carInfoDone = function() {
+      var make  = $scope.car.make;
+      var model = $scope.car.model;
+      var year  = $scope.car.year;
+      var miles = $scope.car.miles;
+      return make !== '' && 
+             model !== '' && 
+             year !== '' && 
+             miles !== '';
+    };
+
+    var leaseInfoDone = function() {
+      var maxMiles            = $scope.lease.maxMiles;
+      var costPerMile         = $scope.lease.costPerMile;
+      var currentMonthInLease = $scope.lease.currentMonthInLease;
+      var lengthOfLease       = $scope.lease.lengthOfLease;
+      return maxMiles !== '' && 
+             costPerMile !== '' && 
+             lengthOfLease !== '' && 
+             currentMonthInLease !== '';
+    };
+
     $scope.prepareSummary = function() {
       $scope.milesDrivenPerYear  = calculateMilesDrivenPerYear();
       $scope.estimatedTotalMiles = estimatedTotalMiles();
       $scope.differenceInMiles   = differenceInMiles();
       $scope.overrageMessage     = ($scope.differenceInMiles < 0 ? calculateCharges() : false); 
-
+      $scope.possibleBudget      = ($scope.suggestedBudget > 0 ? true : false);
       $scope.switchLeaseInfoSummary();
     };
 
 
     $scope.switchCustomerCarInfo = function() {
-      $scope.customerInfo = !$scope.customerInfo;
-      $scope.carInfo      = !$scope.carInfo;
+      if (customerInfoDone()) {
+        $scope.customerInfo = !$scope.customerInfo;
+        $scope.carInfo      = !$scope.carInfo;
+      } else {
+        $scope.toastIt();
+      }
     };
 
     $scope.switchCarLeaseInfo = function() {
-      $scope.carInfo   = !$scope.carInfo;
-      $scope.leaseInfo = !$scope.leaseInfo;
+      if (carInfoDone()) {
+        $scope.carInfo   = !$scope.carInfo;
+        $scope.leaseInfo = !$scope.leaseInfo;
+      } else {
+        $scope.toastIt();
+      }
     };
 
     $scope.switchLeaseInfoSummary = function() {
-      $scope.leaseSummary = !$scope.leaseSummary;
-      $scope.leaseInfo    = !$scope.leaseInfo;
+      if (leaseInfoDone()) {
+        $scope.leaseSummary = !$scope.leaseSummary;
+        $scope.leaseInfo    = !$scope.leaseInfo;
+      } else {
+        $scope.toastIt();
+      }
     };
   });
